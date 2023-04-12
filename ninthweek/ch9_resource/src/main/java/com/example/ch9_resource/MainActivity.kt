3c97f4bce32cc5d2cd90a5e9eacbfcfc5fc2d9b8
yeahzxnn <yejin0621@tukorea.ac.kr>
2023-04-12 20:06:43 +0900

    09 리소스 활용하기
    메신저 앱의 인트로 화면 만들기

---
+++ ninthweek/ch9_resource/src/main/java/com/example/ch9_resource/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.ch9_resource
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = Activi
+        setContentView(R.layout.activity_main)
+    }
+}
